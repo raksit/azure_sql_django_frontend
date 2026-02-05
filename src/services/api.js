5571d0222415b64b1fc7b3f9a9c5e11624bbc2da
phrn47 <pharun.por@gmail.com>
2026-02-05 16:28:31 +0700

    commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://bearlab-backend-d9awbhgfe4c6abdq.southeastasia-01.azurewebsites.net/api',
+  baseURL: 'pharun-frontend-dqe2crhhe0gfdaf9.japanwest-01.azurewebsites.net',
   // baseURL: 'http://127.0.0.1:8000/api', // Local API
   headers: {
     'Content-Type': 'application/json',
